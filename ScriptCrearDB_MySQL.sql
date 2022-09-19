d9cdb4581824c6946aa414a67af96489d985635c
juliodieguez8a <juliodieguez8a@gmail.com>
2022-09-19 00:13:07 -0600

    Add files via upload

---
+++ ScriptCrearDB_MySQL.sql
@@ -0,0 +1,11 @@
+create table cmms_reg.mantoData (
+    ID_Equipo varchar(255),
+    Descripcion_Manto varchar(1000),
+    Fecha_Inicio varchar(255),
+    Hora_Inicio varchar(255),
+	Hora_Fin varchar(255),
+    Planificado varchar(255),
+	Rec_Semanas varchar(255),
+    Rec_Dias varchar(255),
+    Rec_Fecha_Fin varchar(255)
+)
